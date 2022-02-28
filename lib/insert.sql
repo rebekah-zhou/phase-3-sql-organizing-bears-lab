41613808acc5ccef67b4b1e87f8a95f2a84db260
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-28 13:49:38 -0800

    commit tree 4c641c3bf239fd2b16efccf54ebae33fa9f6b583

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 3, 'M', 'brown', 'grumpy', 0),
+("Rowdy", 3, 'M', 'brown', 'grumpy', 0),
+("Tabitha", 3, 'F', 'brown', 'grumpy', 0),
+("Sergeant Brown", 3, 'M', 'brown', 'grumpy', 0),
+("Melissa", 3, 'F', 'brown', 'grumpy', 0),
+("Grinch", 3, 'M', 'brown', 'grumpy', 0),
+("Wendy", 3, 'F', 'brown', 'grumpy', 0),
+(NULL, 3, 'M', 'brown', 'grumpy', 0);
